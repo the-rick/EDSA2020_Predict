93365d52956b8b0c1bef38a5ea7687f09d0da859
Mandlana Nceba <40720517+the-rick@users.noreply.github.com>
2020-02-18 15:45:56 +0200

    Update metricsPackage.py
    added function 4

--- predictpackage/metricsPackage.py
+++ predictpackage/metricsPackage.py
@@ -74,4 +74,40 @@ def date_parser(dates):
     no_time=[dt.split()[0] for dt in dates]
     return no_time
 
+def extract_municipality_hashtags(df):
+    # your code here
+    
+    def mun_func(df):
+        muns = []
+        for d_ in mun_dict.keys():
+            muns.append(d_)
+        for item in muns:
+            if item in df:
+                return mun_dict[item]
+            else:
+                return np.nan
+    
+    def hash_tags(df):
+
+            hash_words = []
+            search_value = '#'
+            
+            if search_value in df:
+                hash_words.append(df.split())
+                hash_tags = []
+                for tag in hash_words[0]:
+                    if search_value in tag:
+                        hash_tags.append(tag.lower())
+                return hash_tags
+            
+            else:
+                return np.nan
+
+    df['municipality'] = df['Tweets'].apply(lambda y: mun_func(y))
+    df['hashtags'] = df['Tweets'].apply(lambda y: hash_tags(y))
+    
+    ret = df[['Tweets', 'Date', 'municipality', 'hashtags']]
+    
+    return ret
+
 
