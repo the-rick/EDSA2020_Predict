33fa31bb6d40e8a0d45604ec8f9afca08376d677
Titus Ndondo <tbndondo@gmail.com>
2020-02-18 11:36:36 +0200

    Initial commit. This commit has only the first function that calculates a dictionary of metrics.

---
+++ tests/test.py
@@ -0,0 +1,8 @@
+from predictpackage import metricsPackage
+
+def dictionary_of_metrics(items):
+    """
+    make sure dictionary_of_metrics works correctly
+    """
+
+    assert metricsPackage.dictionary_of_metrics([39660.0, 36024.0, 32127.0, 39488.0, 18422.0, 23532.0, 8842.0, 37416.0, 16156.0, 18730.0, 19261.0, 25275.0]) == [8, 7, 4], 'incorrect'
