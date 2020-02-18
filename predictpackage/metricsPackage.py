4e764663e404a0122b3579f9281ad7cf8ab32e3c
Titus Ndondo <tbndondo@gmail.com>
2020-02-18 15:07:37 +0200

    Added the package setup.py file and removed the hello function

--- predictpackage/metricsPackage.py
+++ predictpackage/metricsPackage.py
@@ -1,12 +1,21 @@
 def dictionary_of_metrics(items):
-    # your code here
+    """ 
+    Returns a dictionary of metrics (mean, median, variance, standard deviation, minimum and maximum)
+    
+    Example output: 
+    gauteng: List of values
+    dictionary_of_metrics(gauteng) == {'mean': 26244.42,
+                                       'median': 24403.5,
+                                       'var': 108160153.17,
+                                       'std': 10400.01,
+                                       'min': 8842.0,
+                                       'max': 39660.0}
+    """
     def find_mean(items):
         denominator = int()
         for item in items:
             denominator += item
         return denominator / len(items)
-    def hello():
-        print("Hello")
 
     def find_median(items):
         if len(items) % 2 != 0:
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='metricsPackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA metrics package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy', 'pandas'],
+    url='https://github.com/the-rick/EDSA2020_Predict',
+    author='titusndondo',
+    author_email='tbndondo@gmail.com'
+)
