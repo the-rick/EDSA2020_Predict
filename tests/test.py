616c89bb8f3edb60d6ac654b1db782ea09ad7958
thembeks <60382247+thembeks@users.noreply.github.com>
2020-02-19 10:44:26 +0200

    added test for date parser, updated setup file

--- tests/test.py
+++ tests/test.py
@@ -6,3 +6,10 @@ def dictionary_of_metrics(items):
     """
 
     assert metricsPackage.dictionary_of_metrics([39660.0, 36024.0, 32127.0, 39488.0, 18422.0, 23532.0, 8842.0, 37416.0, 16156.0, 18730.0, 19261.0, 25275.0]) == [8, 7, 4], 'incorrect'
+
+def date_parser(dates):
+    """
+    make sure date_parser works correctly
+    """
+
+    assert date_parser(dates[:3]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
